a1e0acadc5353ad0e368efc6d680d8e666841a52
mdanneil <ddewee92@gmail.com>
2023-10-31 20:14:40 +0100

    responsive, ugly header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,97 @@
-import React from 'react'
+import * as React from 'react'
 import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import Drawer from '@mui/material/Drawer'
+import IconButton from '@mui/material/IconButton'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import MenuIcon from '@mui/icons-material/Menu'
+
+const drawerWidth = 240
+const navItems = ['About', 'Projects', 'Contact']
+
 const Header = () => {
+	const [mobileOpen, setMobileOpen] = React.useState(false)
+
+	const handleDrawerToggle = () => {
+		setMobileOpen((prevState) => !prevState)
+	}
+
+	const drawer = (
+		<Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+			<Typography variant='h6' sx={{ my: 2 }}>
+				Marcus
+			</Typography>
+			<Divider />
+			<List>
+				{navItems.map((item) => (
+					<ListItem key={item} disablePadding>
+						<ListItemButton sx={{ textAlign: 'center' }}>
+							<ListItemText primary={item} />
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List>
+		</Box>
+	)
+
 	return (
-		<AppBar position='static'>
-			<Toolbar>
-				<Typography variant='h6'>Marcus</Typography>
-			</Toolbar>
-		</AppBar>
+		<Box sx={{ display: 'flex' }}>
+			<CssBaseline />
+			<AppBar component='nav'>
+				<Toolbar>
+					<IconButton
+						color='inherit'
+						aria-label='open drawer'
+						edge='start'
+						onClick={handleDrawerToggle}
+						sx={{ mr: 2, display: { sm: 'none' } }}>
+						<MenuIcon />
+					</IconButton>
+					<Typography
+						variant='h6'
+						component='div'
+						sx={{
+							flexGrow: 1,
+							display: { xs: 'none', sm: 'block' },
+						}}>
+						Marcus
+					</Typography>
+					<Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+						{navItems.map((item) => (
+							<Button key={item} sx={{ color: '#CDCDCD' }}>
+								{item}
+							</Button>
+						))}
+					</Box>
+				</Toolbar>
+			</AppBar>
+			<nav>
+				<Drawer
+					variant='temporary'
+					open={mobileOpen}
+					onClose={handleDrawerToggle}
+					ModalProps={{
+						keepMounted: true,
+					}}
+					sx={{
+						display: { xs: 'block', sm: 'none' },
+						'& .MuiDrawer-paper': {
+							boxSizing: 'border-box',
+							width: drawerWidth,
+						},
+					}}>
+					{drawer}
+				</Drawer>
+			</nav>
+		</Box>
 	)
 }
 
