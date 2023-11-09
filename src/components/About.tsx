3ac6dceba473b10035698c237a16dfe4392b5c4d
Marcus.W <marcus.w@laggelee.se>
2023-11-09 20:45:22 +0100

    first draft translations

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,8 +13,12 @@ const About = () => {
         alignItems="center"
         style={{ height: "100vh" }}
       >
-        <Grid item>
+        <Grid item width={"50%"} textAlign={"center"}>
           <Typography variant="h5">{t("About.about")}</Typography>
+          <p></p>
+          <Typography variant="h5">{t("About.personality")}</Typography>
+          <p></p>
+          <Typography variant="h5">{t("About.interests")}</Typography>
         </Grid>
       </Grid>
     </section>
