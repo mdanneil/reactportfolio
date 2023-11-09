f1f927cc8cd79177bc3f78003d04e8b190e20ade
Marcus.W <marcus.w@laggelee.se>
2023-11-09 21:09:47 +0100

    add image and ability to deploy to github pages for now

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
 import { Grid, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import React from "react";
+const imagePath: string = require("../assets/pfpimg.jpg");
 
 const About = () => {
   const { t } = useTranslation();
@@ -9,11 +10,36 @@ const About = () => {
     <section id="om mig">
       <Grid
         container
+        direction={"column"}
         justifyContent="center"
         alignItems="center"
         style={{ height: "100vh" }}
       >
-        <Grid item width={"50%"} textAlign={"center"}>
+        <div
+          style={{
+            width: "300px", // Set your desired width
+            height: "350px", // Set your desired height
+            borderRadius: "20%", // Make it round
+            overflow: "hidden", // Hide overflow to ensure a perfect circle
+            marginBottom: "20px", // Add margin for spacing
+          }}
+        >
+          <img
+            src={imagePath}
+            alt="Profile"
+            style={{
+              width: "100%", // Take up 100% of the container
+              height: "auto", // Maintain aspect ratio
+              display: "block", // Remove any default spacing issues
+            }}
+          />
+        </div>
+        <Grid
+          item
+          width={"50%"}
+          textAlign={"center"}
+          sx={{ backgroundColor: "#B0B0B0", borderRadius: "10px" }}
+        >
           <Typography variant="h5">{t("About.about")}</Typography>
           <p></p>
           <Typography variant="h5">{t("About.personality")}</Typography>
