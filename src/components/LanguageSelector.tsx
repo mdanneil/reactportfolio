4ba4a95112da42ec403fdc2e16e7d5d91d5d0f4b
Marcus.W <marcus.w@laggelee.se>
2024-02-28 15:05:54 +0100

    change languageselector from switch to clickable image

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,4 +1,4 @@
-import { Switch, styled } from "@mui/material";
+import { Button } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
 const LanguageSelector = () => {
@@ -8,58 +8,33 @@ const LanguageSelector = () => {
     i18n.changeLanguage(lng);
   };
 
-  const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-    width: 62,
-    height: 34,
-    padding: 7,
-    "& .MuiSwitch-switchBase": {
-      margin: 1,
-      padding: 0,
-      transform: "translateX(6px)",
-      "&.Mui-checked": {
-        color: "#fff",
-        transform: "translateX(22px)",
-        "& .MuiSwitch-thumb:before": {
-          backgroundImage: `url('https://img.icons8.com/color/48/sweden-circular.png')`,
-        },
-        "& + .MuiSwitch-track": {
-          opacity: 1,
-          backgroundColor:
-            theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
-        },
-      },
-    },
-    "& .MuiSwitch-thumb": {
-      backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
-      width: 32,
-      height: 32,
-      "&:before": {
-        content: "''",
-        position: "absolute",
-        width: "100%",
-        height: "100%",
-        left: 0,
-        top: 0,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundImage: `url('https://img.icons8.com/color/48/great-britain.png')`,
-      },
-    },
-    "& .MuiSwitch-track": {
-      opacity: 1,
-      backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
-      borderRadius: 20 / 2,
-    },
-  }));
-
   return (
-    <MaterialUISwitch
-      sx={{ m: 2 }}
-      defaultChecked={i18n.language === "en"}
-      onChange={() => {
+    <Button
+      onClick={() => {
         changeLanguage(i18n.language === "en" ? "sv" : "en");
       }}
-    />
+      style={{
+        position: "fixed",
+        bottom: 0,
+        width: "60px",
+        height: "60px",
+        padding: 5,
+      }}
+    >
+      {i18n.language === "en" ? (
+        <img
+          src="https://img.icons8.com/color/48/great-britain-circular.png"
+          alt="British flag"
+          style={{ width: "100%", height: "100%" }}
+        />
+      ) : (
+        <img
+          src="https://img.icons8.com/color/48/sweden-circular.png"
+          alt="Swedish flag"
+          style={{ width: "100%", height: "100%" }}
+        />
+      )}
+    </Button>
   );
 };
 
